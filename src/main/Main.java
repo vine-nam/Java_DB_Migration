fbad9b798957421369b8d776472557ac43f19e69
vine-nam <wldkdkdk7@gmail.com>
2019-09-05 14:42:53 +0900

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,61 +5,86 @@ import java.util.List;
 
 public class Main {
 
+	final private static String pattern = ",[ |\t]*(?i)where";
+
 	public static void main(String[] args) {
 
 		String migrationFile = args[0];
 		String dbConnectionFile = args[1];
-		
-		if(args.length >= 3) {
-			DBExecute.batchSize = Integer.parseInt(args[2]); 
+
+		if (args.length >= 3) {
+			DBExecute.batchSize = Integer.parseInt(args[2]);
 		}
-		
+
 		DBExecute dbe = null;
 		ReadFile rf = new ReadFile();
 		List<String> lines;
-		String dbNameS = "";
-		String dbNameT = "";
-		
+		String tempSourceDBName = "";
+		String tempTargetDBName = "";
+
 		try {
 			dbe = new DBExecute(dbConnectionFile);
 			lines = rf.readFileExceptCommend(migrationFile);
 			for (String line : lines) {
 				Runtime.getRuntime().gc();
 				String item[] = rf.splitLine(line);
-				if (item.length >= 4) {
-					if(dbNameS.isEmpty() || !dbNameS.equals(item[0])) {
-						dbNameS = item[0];
-						dbe.closeConnection(dbe.conS);
-						dbe.setSourceConnection(dbNameS);
-						if(dbe.conS == null) {
-							Com.printLog("Connection 실패: " + dbNameS);
-							dbNameS = null;
+				if (item.length >= 5) {
+					Com.printLog(item[1] + " → " + item[3] + " (" + item[4] + ")");
+					if (isNotSame(tempSourceDBName, item[0])) {
+						try {
+							dbe.closeSourceConnection();
+							dbe.setSourceConnection(item[0]);
+							tempSourceDBName = item[0];
+						} catch (Exception e) {
+							tempSourceDBName = "";
+							Com.printErrorLog(e.getMessage());
 							continue;
 						}
 					}
-					if(dbNameT.isEmpty() || !dbNameT.equals(item[2])) {
-						dbNameT = item[2];
-						dbe.closeConnection(dbe.conT);
-						dbe.setTargetConnection(dbNameT);
-						if(dbe.conT == null) {
-							Com.printLog("Connection 실패: " + dbNameT);
-							dbNameT = null;
+					if (isNotSame(tempTargetDBName, item[2])) {
+						try {
+							dbe.closeTargetConnection();
+							dbe.setTargetConnection(item[2]);
+							tempTargetDBName = item[2];
+						} catch (Exception e) {
+							tempTargetDBName = "";
+							Com.printErrorLog(e.getMessage());
 							continue;
 						}
 					}
-					Com.printLog(item[1] + " -> " + item[3]);
-					dbe.executeMigration(item[1], item[3]);
+					try {
+						dbe.setConditionalStatment(getConditionalStatment(item, line));
+						dbe.executeMigration(item[0], item[1], item[3]);
+					} catch (Exception e) {
+						Com.printErrorLog(e.getMessage());
+					}
 				}
+				System.out.println();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				dbe.closeConnection(dbe.conS);
-				dbe.closeConnection(dbe.conT);
+				dbe.closeSourceConnection();
+				dbe.closeTargetConnection();
 			} catch (SQLException e) {
 			}
 		}
+	}
+
+	private static boolean isNotSame(String dbName, String inputDBName) {
+		if (dbName.isEmpty() || !dbName.equals(inputDBName)) {
+			return true;
+		}
+		return false;
+	}
+
+	private static String getConditionalStatment(String[] item, String line) {
+		String[] conditions = line.split(pattern);
+		if (conditions.length > 1) {
+			return conditions[1];
+		}
+		return null;
 	}
 
 }
